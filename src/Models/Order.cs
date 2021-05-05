ac0fe1e2ed6ec3216b972c8c864d374d6bde41ea
Mykola Fedurko <mykolaf@compatibl.com>
2021-05-05 22:09:35 +0300

    Added models.

---
+++ src/Models/Order.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ConfectineryShop.Models
+{
+    public class Order
+    {
+        public int ID { get; set; }
+        public int Amount { get; set; }
+
+        public int ProductID { get; set; }
+        public int UserID { get; set; }
+        public Product Product { get; set; }
+        public User User { get; set; }
+
+    }
+}
---
+++ src/Models/User.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ConfectineryShop.Models
+{
+    public class User
+    {
+        [MaxLength(50)]
+        public string FirstName { get; set; }
+        [MaxLength(50)]
+        public string LastName { get; set; }
+
+        public ICollection<Order> Orders { get; set; }
+    }
+}
